62e29a534da232099cba8deb6ad4416c94f1859a
Peter Argue <89119817+peterargue@users.noreply.github.com>
2022-10-14 09:06:27 -0700

    fix a missed import

--- client/internal/blockpresencemanager/blockpresencemanager_test.go
+++ client/internal/blockpresencemanager/blockpresencemanager_test.go
@@ -3,8 +3,8 @@ package blockpresencemanager
 import (
 	"testing"
 
-	"github.com/ipfs/go-bitswap/internal/testutil"
 	peer "github.com/libp2p/go-libp2p/core/peer"
+	"github.com/onflow/go-bitswap/internal/testutil"
 
 	cid "github.com/ipfs/go-cid"
 )
